13e3d6d092ffe59fc2de1eae52dfe622b5883e1b
JashThakkar <jashjt05@gmail.com>
2025-10-01 18:18:24 -0400

    part 4

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ class FadingTextAnimation extends StatefulWidget {
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  bool _showFrame = false;
+
   void toggleVisibility() {
     setState(() {
       _isVisible = !_isVisible;
@@ -28,15 +30,40 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Fading Text Animation')),
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          curve: Curves.easeInOut,
-          duration: Duration(seconds: 1),
-          child: 
-          const Text('Hello, Flutter!', 
-          style: TextStyle(fontSize: 24)),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              curve: Curves.easeInOut,
+              duration: Duration(seconds: 1),
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: _showFrame
+                    ? BoxDecoration(
+                        border: Border.all(color: Colors.blue, width: 3),
+                        borderRadius: BorderRadius.circular(8),
+                      )
+                    : null,
+                child: const Text(
+                  'Hello, Flutter!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 350),
+          SwitchListTile(
+            title: Text('Show Frame', textAlign: TextAlign.right),
+            value: _showFrame,
+            onChanged: (bool value) {
+              setState(() {
+                _showFrame = value;
+              });
+            },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: toggleVisibility,
